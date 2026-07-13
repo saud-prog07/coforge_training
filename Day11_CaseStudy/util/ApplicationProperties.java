1add1047ad95b6380eeb782b9bda594b8cd45cfa
Syed Saud Sayeed <saud.sayeed261005@gmail.com>
2026-07-13 12:45:21 +0530

    Add files via upload

---
+++ Day11_CaseStudy/util/ApplicationProperties.java
@@ -0,0 +1,25 @@
+package com.coforge.ems.util;
+
+public class ApplicationProperties {
+	// Database Constants
+	public static final String driver = "com.mysql.cj.jdbc.Driver";
+	public static final String url = "jdbc:mysql://localhost:3306/db_employee";
+	public static final String root = "root";
+	public static final String password = "Saud231005@";
+
+	// Databse Sql queries
+	public static final String insert = "insert into employee values(?, ?, ?, ?)";
+	public static final String findAll = "select * from employee";
+	public static final String findEmployee = "select eid,ename,esalary,dno from employee where eid = ? ";
+	public static final String updateSalary = "update employee set esalary = ? where eid= ?";
+	public static final String delete = "delete from employee where eid= ?";
+	public static final String updateDeptNo = "update employee set dno = ? where eid= ?";
+
+	// Application Response Properties
+	public static final String insertSuccess = "Success : Employee Record Inserted";
+	public static String dbFailed = "Failed : DB ERROR";
+
+	// Application Validation Failed
+
+	public static final String validationFailed = "Failed:Invalid Employee Details";
+}
