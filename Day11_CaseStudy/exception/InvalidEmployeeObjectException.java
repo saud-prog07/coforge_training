dfdf3c6bab13c41a0ae265c8cff48efe65c8d379
Syed Saud Sayeed <saud.sayeed261005@gmail.com>
2026-07-13 12:45:53 +0530

    Delete Day11_CaseStudy directory

--- Day11_CaseStudy/exception/InvalidEmployeeObjectException.java
+++
@@ -1,9 +0,0 @@
-package com.coforge.ems.exception;
-
-import com.coforge.ems.util.ApplicationProperties;
-
-public class InvalidEmployeeObjectException extends Exception{
-	    public String toString() {
-	        return ApplicationProperties.validationFailed;
-	    }
-}
