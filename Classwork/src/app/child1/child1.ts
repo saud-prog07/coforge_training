a50c5d0299e7b7cdf96e110ea28bcb08e689a173
Syed Saud Sayeed <saud.sayeed261005@gmail.com>
2026-07-30 15:09:48 +0530

    Delete Classwork/src directory

--- Classwork/src/app/child1/child1.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-child1',
-  standalone: false,
-  templateUrl: './child1.html',
-  styleUrl: './child1.css',
-})
-export class Child1 {}
